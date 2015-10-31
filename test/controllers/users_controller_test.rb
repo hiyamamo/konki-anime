5ca266509fc0680fef0e84301bd420c6df37362f
hiyamamo <h1r0ki_yamam0t0@yahoo.co.jp>
2015-10-31 18:23:03 +0900

    fixed show test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
 	test 'show' do
 		user = users(:one)
+		sign_in user
 		get :show, { :user_name => user.name }
 		assert_response :success
 	end
