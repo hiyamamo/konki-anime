6ef5a9bf152656af9088d29e43cadc13565fe558
hiyamamo <h1r0ki_yamam0t0@yahoo.co.jp>
2015-11-03 15:21:19 +0900

    何も新規作成時に選択されなかった時の処理追加

--- app/controllers/watch_lists_controller.rb
+++ app/controllers/watch_lists_controller.rb
@@ -10,30 +10,35 @@ class WatchListsController < ApplicationController
 	end
 
 	def new
-		@season = Season.where(:current => true).first
+		@season = Season.current
+    @programs = Program.where(:season => @season)
 	end
 
 	def create
 		user = User.find_by_name(params[:user_name])
 		detail_ids = params[:selection]
 
-		# 新規のみ追加
-		detail_ids.each do |detail_id|
-			if user.watch_lists.where(:detail_id => detail_id).empty?
-				user.watch_lists.create(:detail_id => detail_id)
-			end
-		end
-		# 既存のIDから無くなったものを削除
-		detail_ids = detail_ids.map do |d|
-			d.to_i
-		end
-		user.watch_lists.each do |watch_list|
-			if watch_list.detail.season == Season.find_by_current(true)
-				unless detail_ids.include? watch_list.detail_id
-					watch_list.destroy
-				end
-			end
-		end
+    watch_lists = user.watch_lists(Season.current)
+
+    unless detail_ids.blank?
+      # 新規のみ追加
+      detail_ids.each do |detail_id|
+        if user.watch_lists.where(:detail_id => detail_id).empty?
+          user.watch_lists.create(:detail_id => detail_id)
+        end
+      end
+      # 既存のIDから無くなったものを削除
+      detail_ids = detail_ids.map do |d|
+        d.to_i
+      end
+      watch_lists.each do |watch_list|
+        unless detail_ids.include? watch_list.detail_id
+          watch_list.destroy
+        end
+      end
+    else
+      watch_lists.destroy_all
+    end
 		redirect_to user_path(:name => user.name)
 	end
 
