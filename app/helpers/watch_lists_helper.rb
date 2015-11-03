c5b2423c26ef24bdee09b181cdd864f30c77e8d3
hiyamamo <h1r0ki_yamam0t0@yahoo.co.jp>
2015-11-03 15:23:41 +0900

    parse_time -> strftime

--- app/helpers/watch_lists_helper.rb
+++ app/helpers/watch_lists_helper.rb
@@ -3,14 +3,10 @@ require 'json'
 require 'date'
 
 module WatchListsHelper
-	def parse_time(start_time)
-		start_time.strftime("%m月%d日 %H:%M")
-	end
-
 	def build_options(details)
 		results = {}
 		details.each do |detail|
-			time = parse_time detail["started_at"]
+			time = strftime detail["started_at"]
 			disp = "#{detail['tv_station']} : #{time}"
 			results[disp] = detail.id
 		end
